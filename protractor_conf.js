e11f70d84a28c9e02ce2f35d8023631e2732fb0b
Janne Kytömäki <janne@addsearch.com>
2015-09-18 12:40:51 +0300

    Updated version

--- protractor_conf.js
+++ protractor_conf.js
@@ -1,24 +1,30 @@
 // An example configuration file.
 exports.config = {
-  allScriptsTimeout: 99999,
-  // Do not start a Selenium Standalone sever - only run this using chrome.
-  directConnect: true,
-  //chromeDriver: './node_modules/protractor/selenium/chromedriver',
+    allScriptsTimeout: 99999,
+    // Do not start a Selenium Standalone sever - only run this using chrome.
+    directConnect: true,
+    chromeDriver: '/usr/lib/node_modules/webdriver-manager/selenium/chromedriver',
 
-  seleniumArgs: ['-browserTimeout=60'],
+    seleniumArgs: ['-browserTimeout=60'],
 
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    'browserName': 'firefox'
-  },
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+        'browserName': 'chrome',
+        firefox_binary: '/usr/bin/firefox',
+        'firefoxOptions': {
+            firefox_binary: '/usr/bin/firefox',
+            args: [],
+            extensions: []
+        }
+    },
 
-  // Spec patterns are relative to the current working directly when
-  // protractor is called.
-  specs: ['test/e2e/**/*.spec.js'],
+    // Spec patterns are relative to the current working directly when
+    // protractor is called.
+    specs: ['test/e2e/**/*.spec.js'],
 
-  // Options to be passed to Jasmine-node.
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-  }
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000,
+    }
 };
